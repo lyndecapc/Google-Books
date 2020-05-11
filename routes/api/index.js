e77c8c7772926bdcb012eed49c1e136a0491c957
Lynde Capogreco <lyndecapc@gmail.com>
2020-05-11 16:11:48 -0500

    updated google controllers

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,13 +3,13 @@ const router = require("express").Router();
 const bookRoutes = require("./books");
 const googleRoutes = require("./google");
 
-// Book routes
+//booko route, google route, home page
 router.use("/books", bookRoutes);
 
-// Google Routes
+
 router.use("/google", googleRoutes);
 
-// For anything else, render the html page
+
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
